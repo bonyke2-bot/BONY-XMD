aadec305eab34d6ad4872efe82f044b4bad14c11
𝐖𝐞𝐞𝐝 𝐓𝐞𝐜𝐡 333🧘‍♂️ <weedtech26@gmail.com>
2026-08-26 23:11:25 -0400

    Update GitHub repository link in repo.js

--- commands/repo.js
+++ commands/repo.js
@@ -7,7 +7,7 @@ module.exports = async (sock, m, { text }) => {
     let repoInfo = `╭━━━〔 *RIFT-MD REPOSITORY* 〕━━━⡱
 ┃ 🤖 *Bot Name:* RIFT-MD
 ┃ 👑 *Owner:* WeedDev
-┃ 📦 *GitHub:* https://github.com/TECHGOAT333/QUEEN-COLOMBIA
+┃ 📦 *GitHub:* https://github.com/WeedTech/RIFT-MD
 ╰━━━━━━━━━━━━━━━━━━━━⬣
 
 > _©️ Powered by RIFT-MD MULTI-DEVICE_`.trim();
@@ -19,7 +19,7 @@ module.exports = async (sock, m, { text }) => {
                 title: "RIFT-MD OFFICIAL REPO",
                 body: "Click here to view the source code",
                 thumbnailUrl: "https://files.catbox.moe/vv674d.jpg",
-                sourceUrl: "https://github.com/WeedTech/---",
+                sourceUrl: "https://github.com/WeedTech/RIFT-MD",
                 mediaType: 1,
                 renderLargerThumbnail: true
             }
