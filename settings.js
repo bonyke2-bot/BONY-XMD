dc1c6f352a2f450e6ed1890dc501706e57e0ef68
𝐖𝐞𝐞𝐝 𝐓𝐞𝐜𝐡 333🧘‍♂️ <weedtech26@gmail.com>
2026-08-29 09:58:17 -0400

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,32 @@
+module.exports = {
+  // 🤖 BASIC INFO
+  botName: "RIFT-MD 👑",
+  ownerName: "WeedTech",
+  ownerNumber: "50948247470",
+  sudoNumbers: ["50948247470"],
+  prefix: ".",
+  version: "2.0.0",
+  
+  // ⚙️ SYSTEM & ENVIRONMENT
+  sessionName: "riftmd-session",
+  timezone: "America",
+  language: "en",
+
+  // 🎨 STICKER UI CUSTOMIZATION
+  packname: "Created by",
+  author: "RIFT-MD 💜",
+
+  // 🛡️ SECURITY & CONTROL OPTIONS
+  antiLink: false,
+  antiSpam: true,
+  autoReadMessages: false,
+  autoReact: true,
+
+  // 🔌 ADVANCED FEATURES & API CONNECTIONS
+  geminiApiKey: "",
+  mongodbUrl: "",
+
+  // 💬 AUTOMATIC MESSAGES
+  welcomeMessage: true,
+  goodbyeMessage: true
+};
