1ddbee99a450468651388458361b7f085bd48f6a
Bony xmd <Ondiechechrispinus0@gmail.com>
2026-09-05 02:42:11 +0300

    Delete pair.js

--- pair.js
+++
@@ -1,3 +0,0 @@
-export default async (sock, m, {from, config})=>{
-  await sock.sendMessage(from, {text: `*🔗 GET BONY-BOT SESSION*\n\nOpen: ${config.pairSite}\n\n1. Enter number 2547...\n2. Get code\n3. Link in WhatsApp\n4. You get BONY-BOT~...`}, {quoted: m});
-}
