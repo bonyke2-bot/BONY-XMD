9bee3548466a6c7796894a406ae261faccd4ad3d
Bony xmd <Ondiechechrispinus0@gmail.com>
2026-09-04 23:22:00 +0300

    Update app.js

--- devbony/app.js
+++ devbony/app.js
@@ -1,1 +1,17 @@
+"use strict";
+// BONY XMD - Powered by bonyxmd.co.ke
+// BONY KE Official - React Function
 
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.reagir = void 0;
+
+async function reagir(dest, zok, msg, emoji) {
+    try {
+        await zok.sendMessage(dest, { react: { text: emoji, key: msg.key } });
+    } catch (e) {
+        console.log("BONY-XMD React Error:", e.message);
+    }
+}
+
+exports.reagir = reagir;
+// Powered by BONY KE | bonyxmd.co.ke
