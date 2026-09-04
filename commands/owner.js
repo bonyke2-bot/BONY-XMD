c812cceeb6fb218a226f0e28967c6ca6a0c4239c
Bony xmd <Ondiechechrispinus0@gmail.com>
2026-09-05 00:08:03 +0300

    Delete commands directory

--- commands/owner.js
+++
@@ -1,3 +0,0 @@
-export default async (sock, m, {from, config})=>{
-  await sock.sendMessage(from, {text: `*👑 OWNER: ${config.ownerName}*\n📱 ${config.ownerNumber[0]}\n🤖 ${config.botName}\n🔗 ${config.pairSite}`}, {quoted: m});
-}
