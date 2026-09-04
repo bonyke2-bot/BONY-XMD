e7b7ea29c0376f3a14f20bec4ff1c2536875f31a
Bony xmd <Ondiechechrispinus0@gmail.com>
2026-09-05 02:43:54 +0300

    Delete alive.js

--- alive.js
+++
@@ -1,3 +0,0 @@
-export default async (sock, m, {from})=>{
-  await sock.sendMessage(from, {text: "*🤖 BONY XMD ALIVE!*\n\n✅ Online\n👑 Owner: BONY KE\n🔗 Pair: https://bony-xmd-pair.onrender.com\n\n*BONY XMD 2026*"}, {quoted: m});
-}
