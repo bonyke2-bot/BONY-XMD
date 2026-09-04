5161bbedeaa72ff141668a7f921e22ac5412f1ad
Bony xmd <Ondiechechrispinus0@gmail.com>
2026-09-05 02:43:33 +0300

    Delete menu.js

--- menu.js
+++
@@ -1,6 +0,0 @@
-export default async (sock, m, {from, config})=>{
-  let menu = `*🤖 BONY XMD MENU*\n\n`;
-  menu+= `• .ping\n• .alive\n• .owner\n• .pair\n• .bony\n`;
-  menu+= `\nPair: ${config.pairSite}\nBy ${config.ownerName}`;
-  await sock.sendMessage(from, {text: menu}, {quoted: m});
-}
