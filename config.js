b06d661ac2734b27bc6179b1c7fd5976b81a2d52
Bony xmd <Ondiechechrispinus0@gmail.com>
2026-09-05 11:02:33 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || '',
+    PREFIX: process.env.PREFIX || '.',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '254748339103',
+    OWNER_NAME: process.env.OWNER_NAME || 'Bonyke',
+    CHANNEL_JID: '120363382023564503@newsletter', // your channel 0029Vb8coEnKAwEcRBDDnq0Z
+    CHANNEL_LINK: 'https://whatsapp.com/channel/0029Vb8coEnKAwEcRBDDnq0Z',
+    GROUP_LINK: 'https://chat.whatsapp.com/BEr0VScxfRDB82TSdo3EWT',
+    GROUP_INVITE_CODE: 'BEr0VScxfRDB82TSdo3EWT'
+}
